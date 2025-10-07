ee0e63b24b8126c37a540128dfef99cc688e7b7f
AkshataDeole <akshatadeole@gmail.com>
2025-10-08 00:26:57 +0530

    Add files via upload

---
+++ UserDefined.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int value1=0, value2=0, ans=0;
+
+    value1=20;
+    value2=30;
+    ans=value1+value2;
+
+    printf("Addition is:\n");
+    scanf("%d", ans);
+    
+
+    return 0;
+}
